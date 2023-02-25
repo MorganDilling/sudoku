1a2c9ff37eba7774c4db3f9740ec8e06f87bf465
Morgan Dilling <morgandilling171@gmail.com>
2023-02-25 18:40:36 +0000

    chore: any type ignored by line

--- src/lib/Instance.ts
+++ src/lib/Instance.ts
@@ -1,13 +1,17 @@
 export default class Instance {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private _onChanged: ((...args: any[]) => void)[];
   onChanged: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     connect: (callback: (...args: any[]) => void) => () => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     fire: (...args: any[]) => void;
   };
 
   constructor() {
     this._onChanged = [];
     this.onChanged = {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       connect: (callback: (...args: any[]) => void) => {
         const length = this._onChanged.length;
         this._onChanged.push(callback);
@@ -16,8 +20,11 @@ export default class Instance {
           this._onChanged.splice(length, 1);
         };
       },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       fire: (...args: any[]) => {
-        this._onChanged.forEach((callback) => setTimeout(()=>callback(...args),0));
+        this._onChanged.forEach((callback) =>
+          setTimeout(() => callback(...args), 0)
+        );
       },
     };
   }
